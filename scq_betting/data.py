c7482214d80817a2090adc7b1a8efa9f3d2ea727
raulsharma21 <114191720+raulsharma21@users.noreply.github.com>
2024-04-28 14:44:13 -0700

    Updating file paths

--- scq_betting/data.py
+++ scq_betting/data.py
@@ -18,29 +18,29 @@ mastertable = pd.read_html(masterUrl)[0]
 
 # parse tables from html - stored as var
 
-mastertable.to_excel("MasterStats.xlsx")
-mastertable = pd.read_excel("MasterStats.xlsx", header=1)
-mastertable.to_excel("MasterStats.xlsx")
+mastertable.to_excel("../__data__/MasterStats.xlsx")
+mastertable = pd.read_excel("../__data__/MasterStats.xlsx", header=1)
+mastertable.to_excel("../__data__/MasterStats.xlsx")
 
 # Store the teams stats in Excel file
 
 
 def offensiveRating(team):
-    oetable = pd.read_excel("MasterStats.xlsx")
+    oetable = pd.read_excel("../__data__/MasterStats.xlsx")
     row_index = oetable.index[oetable["Team"] == team].tolist()
 
     return oetable["ORtg"].iloc[row_index[0]]
 
 
 def defensiveRating(team):
-    detable = pd.read_excel("MasterStats.xlsx")
+    detable = pd.read_excel("../__data__/MasterStats.xlsx")
     row_index = detable.index[detable["Team"] == team].tolist()
 
     return detable["DRtg"].iloc[row_index[0]]
 
 
 def pointsPerGame(team):
-    ppgtable = pd.read_excel("MasterStats.xlsx")
+    ppgtable = pd.read_excel("../__data__/MasterStats.xlsx")
     row_index = ppgtable.index[ppgtable["Team"] == team].tolist()
 
     return (
@@ -51,70 +51,70 @@ def pointsPerGame(team):
 
 
 def trueShootingPercentage(team):
-    tsptable = pd.read_excel("MasterStats.xlsx")
+    tsptable = pd.read_excel("../__data__/MasterStats.xlsx")
     row_index = tsptable.index[tsptable["Team"] == team].tolist()
 
     return tsptable["TS%"].iloc[row_index[0]]
 
 
 def turnoverPercentage(team):
-    totable = pd.read_excel("MasterStats.xlsx")
+    totable = pd.read_excel("../__data__/MasterStats.xlsx")
     row_index = totable.index[totable["Team"] == team].tolist()
 
     return totable["TOV%"].iloc[row_index[0]]
 
 
 def marginOfVictory(team):
-    asmtable = pd.read_excel("MasterStats.xlsx")
+    asmtable = pd.read_excel("../__data__/MasterStats.xlsx")
     row_index = asmtable.index[asmtable["Team"] == team].tolist()
 
     return asmtable["MOV"].iloc[row_index[0]]
 
 
 def winPercentage(team):
-    wpmtable = pd.read_excel("MasterStats.xlsx")
+    wpmtable = pd.read_excel("../__data__/MasterStats.xlsx")
     row_index = wpmtable.index[wpmtable["Team"] == team].tolist()
 
     return wpmtable["W/L%"].iloc[row_index[0]]
 
 
 def strengthOfSchedule(team):
-    stptable = pd.read_excel("MasterStats.xlsx")
+    stptable = pd.read_excel("../__data__/MasterStats.xlsx")
     row_index = stptable.index[stptable["Team"] == team].tolist()
 
     return (stptable["SOS"]).iloc[row_index[0]]
 
 
 def simpleRating(team):
-    rattable = pd.read_excel("MasterStats.xlsx")
+    rattable = pd.read_excel("../__data__/MasterStats.xlsx")
     row_index = rattable.index[rattable["Team"] == team].tolist()
 
     return (rattable["SRS"]).iloc[row_index[0]]
 
 
 def freeThrowAttemptRate(team):
-    ftrttable = pd.read_excel("MasterStats.xlsx")
+    ftrttable = pd.read_excel("../__data__/MasterStats.xlsx")
     row_index = ftrttable.index[ftrttable["Team"] == team].tolist()
 
     return (ftrttable["FTr"]).iloc[row_index[0]]
 
 
 def freeThrowPercentage(team):
-    ftptable = pd.read_excel("MasterStats.xlsx")
+    ftptable = pd.read_excel("../__data__/MasterStats.xlsx")
     row_index = ftptable.index[ftptable["Team"] == team].tolist()
 
     return (ftptable["FT%"]).iloc[row_index[0]]
 
 
 def offensiveReboundPercentage(team):
-    orptable = pd.read_excel("MasterStats.xlsx")
+    orptable = pd.read_excel("../__data__/MasterStats.xlsx")
     row_index = orptable.index[orptable["Team"] == team].tolist()
 
     return (orptable["ORB%"]).iloc[row_index[0]]
 
 
 def defensiveReboundPercentage(team):
-    drptable = pd.read_excel("MasterStats.xlsx")
+    drptable = pd.read_excel("../__data__/MasterStats.xlsx")
     row_index = drptable.index[drptable["Team"] == team].tolist()
 
     return (drptable["DRB%"]).iloc[row_index[0]]
